6c4154dfd99a0099dd92fcffb3681c96b537581f
Sheikh-Nightshader <gillesderaisboy@gmail.com>
2024-12-06 21:44:07 -0500

    Update pyenkoder.py

--- pyenkoder.py
+++ pyenkoder.py
@@ -1,17 +1,16 @@
 import base64
 import gzip
-import argparse
 import random
 
 def print_banner():
-    banner = """
- ____        _____                     _
-|  _ \ _   _| ____|_ __   ___ ___   __| | ___ _ __
-| |_) | | | |  _| | '_ \ / __/ _ \ / _` |/ _ \ '__|
-|  __/| |_| | |___| | | | (_| (_) | (_| |  __/ |
-|_|    \__, |_____|_| |_|\___\___/ \__,_|\___|_|
+    banner = r"""
+ ____        _____       _  __         _
+|  _ \ _   _| ____|_ __ | |/ /___   __| | ___ _ __
+| |_) | | | |  _| | '_ \| ' // _ \ / _` |/ _ \ '__|
+|  __/| |_| | |___| | | | . \ (_) | (_| |  __/ |
+|_|    \__, |_____|_| |_|_|\_\___/ \__,_|\___|_|
        |___/
-     Python Script Encoder, Sheikh Nightshader
+   Python Script Obfuscator, Sheikh Nightshader
     """
     colors = [31, 32, 33, 34, 35, 36]
     for line in banner.split("\n"):
@@ -29,21 +28,19 @@ exec(gzip.decompress(base64.b64decode("{encoded_content}")))
 '''
         return obfuscated_script
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Error :( : {e}")
         return None
 
 def main():
     print_banner()
-    parser = argparse.ArgumentParser(description="Encode a Python script with Base64 and Gzip.")
-    parser.add_argument("file", help="Path to the Python file to encode.")
-    args = parser.parse_args()
+    file_path = input("Python script to encode: ").strip()
 
-    obfuscated_script = encode_script(args.file)
+    obfuscated_script = encode_script(file_path)
     if obfuscated_script:
-        print("\n[+] Encoded Script:\n")
+        print("\n[+] Encoded Script :) :\n")
         print(obfuscated_script)
     else:
-        print("\n[-] Failed to encode the script. Check the file path or file format.")
+        print("\n[-] Failed to Encode :( ")
 
 if __name__ == "__main__":
     main()
