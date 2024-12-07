13fe2d8d680be938557a3a49691c2c8ea9fec044
Sheikh-Nightshader <gillesderaisboy@gmail.com>
2024-12-06 21:45:03 -0500

    Update pykrypt.py

--- pykrypt.py
+++ pykrypt.py
@@ -3,13 +3,13 @@ from cryptography.fernet import Fernet
 
 def print_banner():
     banner = r"""
- ____        _____                             _
-|  _ \ _   _| ____|_ __   ___ _ __ _   _ _ __ | |_
-| |_) | | | |  _| | '_ \ / __| '__| | | | '_ \| __|
-|  __/| |_| | |___| | | | (__| |  | |_| | |_) | |_
-|_|    \__, |_____|_| |_|\___|_|   \__, | .__/ \__|
-       |___/                       |___/|_|
-     Python Script Encryptor, Sheikh Nightshader
+ ____        _  __                 _
+|  _ \ _   _| |/ /_ __ _   _ _ __ | |_
+| |_) | | | | ' /| '__| | | | '_ \| __|
+|  __/| |_| | . \| |  | |_| | |_) | |_
+|_|    \__, |_|\_\_|   \__, | .__/ \__|
+       |___/           |___/|_|
+Python Script Enkryptor, Sheikh Nightshader
     """
     colors = [31, 32, 33, 34, 35, 36]
     for line in banner.split("\n"):
@@ -36,22 +36,22 @@ def save_encrypted_script(output_path, encrypted_script):
     try:
         with open(output_path, "w") as f:
             f.write(encrypted_script)
-        print(f"[+] Encrypted script saved to: {output_path}")
+        print(f"[+] Enkrypted script saved to: {output_path}")
     except Exception as e:
-        print(f"Error saving encrypted script: {e}")
+        print(f"Error :( : {e}")
 
 def main():
     print_banner()
-    file_path = input("Python script to encrypt: ")
+    file_path = input("Python script to enkrypt: ")
 
     key = Fernet.generate_key()
     encrypted_script = encrypt_script(file_path, key)
     if encrypted_script:
-        output_path = file_path + ".crypted.py"
+        output_path = file_path + ".krypt.py"
         save_encrypted_script(output_path, encrypted_script)
-        print(f"\n[+] Encrypted the script :D saved to: {output_path}")
+        print(f"\n[+] Enkrypted the script :D saved to: {output_path}")
     else:
-        print("\n[-] Failed to encrypt :( Check the file path or file format.")
+        print("\n[-] Failed to enkrypt :(")
 
 if __name__ == "__main__":
     main()
